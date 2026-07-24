49c467f4ce2c0fe5e2b822441b9d980475d09eb7
Awah-Stercy <awahstercy@gmail.com>
2026-07-24 00:54:45 -0700

    create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let fellows = [
+    {
+        name: "Shez",
+        role: "Frontend Developer",
+        points: 95
+    },
+    {
+        name: "Rinz",
+        role: "Backend Developer",
+        points: 90
+    },
+    {
+        name: "Alberta",
+        role: "UI/UX Designer",
+        points: 87
+    }
+];
+for (let i = 0; i < fellows.length; i++)
+{
+    console.log(
+        "Name: "+ fellows[i].name +
+        ", Role: "+ fellows[i].role +
+        ", Points: "+ fellows[i].points
+    );
+}
